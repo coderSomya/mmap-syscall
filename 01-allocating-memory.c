8e3e412e5e116fc7f1e49d58e2cb3cabb27e4e94
somya-weilliptic <somya@weilliptic.com>
2024-09-26 11:58:10 +0530

    allocate mem using mmap

---
+++ 01-allocating-memory.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <stdio.h>
+#include <sys/mman.h>
+
+#define N 10
+
+int main(int argc, char *argv[])
+{
+    int *xs = mmap(NULL, N* sizeof(int), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+
+    assert(xs!= MAP_FAILED);
+
+    assert(N>=2);
+    xs[0] = 1;
+    xs[1] = 1;
+
+    for (int i=2; i<N; i++){
+        xs[i]=xs[i-1]+xs[i-2];
+    }
+
+    int err= munmap(xs, N*sizeof(int));
+
+    assert(err>=0);
+
+    printf("hello world..!");
+    return 0;
+}
---
+++ Makefile
@@ -0,0 +1,2 @@
+01-allocating-memory: 01-allocating-memory.c
+	gcc -Wall -ggdb -o 01-allocating-memory.exe 01-allocating-memory.c
