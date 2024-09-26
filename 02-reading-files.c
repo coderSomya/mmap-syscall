b3255fd1ff924311530ab6ea82e821bb5cdca953
somya-weilliptic <somya@weilliptic.com>
2024-09-26 14:41:15 +0530

    change contents of a file

--- 02-reading-files.c
+++ 02-reading-files.c
@@ -13,16 +13,27 @@ int main(int argc, char *argv[]){
     assert(argc>=2);
     const char *filepath = argv[1];
 
-    int fd = open(filepath, O_RDONLY);
+    int fd = open(filepath, O_RDWR);
     assert(fd>=0);
 
     struct stat statbuff;
     int err = fstat(fd, &statbuff);
     assert(err>=0);
 
-    int *ptr = mmap(NULL, statbuff.st_size, PROT_READ, MAP_PRIVATE , fd, 0);
+    //if u dont use MAP_SHARED, writing wont change the original file
+    char *ptr = mmap(NULL, statbuff.st_size, PROT_READ | PROT_WRITE, MAP_SHARED , fd, 0);
     assert(ptr!= MAP_FAILED);
     close(fd);
+
+
+    //reverse contents of a file
+
+    for (ssize_t i=0; i<statbuff.st_size/2; i++){
+        int j = statbuff.st_size-1-i;
+        int t = ptr[i];
+        ptr[i]=ptr[j];
+        ptr[j]=t;
+    }
 
     // 1 is writing to stdout
     ssize_t n = write(1, ptr, statbuff.st_size);
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ __can be used for the following things__
 
 ```
 1) Allocating memory (who needs malloc, duh!)
-2) Reading from files
+2) Reading from/writing to  files
 3) Inter process communication
 4) Precise control over memory protection
 ```
